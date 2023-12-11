26d3ae956f430a290c70cce3a32f802a9c06c9c9
contratojlm <150732484+contratojlm@users.noreply.github.com>
2023-12-11 03:57:00 -0300

    Add files via upload

---
+++ js/scroll-test.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const targetSection = document.getElementById("geradorbotao");
+
+  const myTest = document.querySelector(".test1");
+  const myTest2 = document.getElementById("test-10");
+
+  setTimeout(function () {
+    myTest.classList.remove("test1");
+    myTest2.removeAttribute("id");
+  }, 350);
+  
+    document.body.style.scrollBehavior = "smooth";
+    document.body.style.scrollSnapType = "mandatory";
+    document.body.style.scrollSnapPointsY = "repeat(100%)";
+  
+    setTimeout(function () {
+      const offset = 11;
+      const targetPosition =
+        targetSection.getBoundingClientRect().top + window.pageYOffset - offset;
+  
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      });
+    }, 1000);
+  });
